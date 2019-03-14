7c08d18f95199321263eb55dadc187bc73f71f73
SK <SK@DESKTOP-ODTOFJV>
2019-03-15 01:18:28 +0300

    Test encapsulation

---
+++ src/javabooks/Author.java
@@ -0,0 +1,19 @@
+package javabooks;
+
+class Author {
+
+    private String name;
+
+    public Author(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
---
+++ src/javabooks/Book.java
@@ -0,0 +1,55 @@
+package javabooks;
+
+public class Book {
+
+    private String title;
+    Author[] authors;
+
+    public static void main(String[] args) {
+
+        Book bookName = new Book();
+
+        Author[] authors = {
+            new Author("Петров"),
+            new Author("Иванов"),
+            new Author("Сидоров")
+        };
+
+        // Тэсты
+        bookName.setTitle("Водолаз в горах Кавказа");
+        System.out.println(bookName.getTitle());
+        System.out.println(bookName.toString());
+        System.out.println(authors[0].getName() + "\n");
+
+        String books[] = new String[5];
+        bookName.setTitle("Баба Яга в тылк врага");
+        books[0] = "Название " + "\"" + bookName.getTitle() + "\"" + "; Автор " + authors[0].getName();
+        bookName.setTitle("На троих");
+        books[1] = "Название " + "\"" + bookName.getTitle() + "\"" + "; Автор " + authors[1].getName();
+        bookName.setTitle("Меняю собаку на паровоз");
+        books[2] = "Название " + "\"" + bookName.getTitle() + "\"" + "; Автор " + authors[2].getName();
+        bookName.setTitle("Воскресный день в аду");
+        books[3] = "Название " + "\"" + bookName.getTitle() + "\"" + "; Автор " + authors[1].getName();
+        bookName.setTitle("Зелёный слоник");
+        authors[0].setName("Пупкин");
+        books[4] = "Название " + "\"" + bookName.getTitle() + "\"" + "; Авторы " + authors[0].getName() +", "+ authors[1].getName();
+
+        for (String ss : books) {
+            System.out.println(ss);
+        }
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return title;
+    }
+}
