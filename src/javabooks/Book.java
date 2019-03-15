f46a8ad53fcaf68485dfed33482b995e5e8cb573
SK <SK@DESKTOP-ODTOFJV>
2019-03-15 10:27:21 +0300

    Test encapsulation

--- src/javabooks/Book.java
+++ src/javabooks/Book.java
@@ -1,27 +1,42 @@
+/**
+ * ДЗ
+ * Создайте класс Book и Author 
+ * Книгу могут написать много авторов 
+ * (в классе Book есть поле Author[] authors) 
+ * Создайте конструкторы, get/set (где необходимо сделайте проверку), 
+ * также метод toString()  
+ * Создайте массив из 5 книг
+ */
 package javabooks;
 
 public class Book {
 
-    private String title;
+    private String title; // Закрытое поле
     Author[] authors;
 
     public static void main(String[] args) {
-
+        
+// Экзэмпляр класса Book 
         Book bookName = new Book();
-
+// Экзэмпляр класса Author 
         Author[] authors = {
             new Author("Петров"),
             new Author("Иванов"),
             new Author("Сидоров")
         };
 
-        // Тэсты
+        // Подгружаем название книги через Settter
         bookName.setTitle("Водолаз в горах Кавказа");
+        // Выводим название книги через Getter
         System.out.println(bookName.getTitle());
+        // Проверка метода toString
         System.out.println(bookName.toString());
+        // Выводим имя автора через Getter
         System.out.println(authors[0].getName() + "\n");
 
+        // Объявляем массив из 5 книг
         String books[] = new String[5];
+        
         bookName.setTitle("Баба Яга в тылк врага");
         books[0] = "Название " + "\"" + bookName.getTitle() + "\"" + "; Автор " + authors[0].getName();
         bookName.setTitle("На троих");
@@ -38,11 +53,11 @@ public class Book {
             System.out.println(ss);
         }
     }
-
+    // Getter
     public String getTitle() {
         return title;
     }
-
+    // Settter
     public void setTitle(String title) {
         this.title = title;
     }
