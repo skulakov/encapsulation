0870aecfa6ac5bd276bb1ca7e58b3bc793e9c015
SK <SK@DESKTOP-ODTOFJV>
2019-03-15 10:27:35 +0300

    Test encapsulation

--- src/javabooks/Author.java
+++ src/javabooks/Author.java
@@ -2,16 +2,19 @@ package javabooks;
 
 class Author {
 
-    private String name;
+    private String name; // Закрытое поле
 
+    // Constructor
     public Author(String name) {
         this.name = name;
     }
 
+    // Getter
     public String getName() {
         return name;
     }
 
+    // Settter
     public void setName(String name) {
         this.name = name;
     }
